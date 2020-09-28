1e723b4e20d254ac7c9694fe4184c166cd604215
mody <mohamed_bouzid@ymai.com>
2020-09-28 13:59:13 +0100

    try fixes

--- lib/railtie.rb
+++ lib/railtie.rb
@@ -3,9 +3,18 @@ require 'rails'
 class WebpackNative::Railtie < ::Rails::Railtie
 
   initializer "webpack_native_set_manifest" do
+
+    def load_webpack_manifest
+      # Set WEBPACK_MANIFEST_PATH to point to the manifest file
+      webpack_manifest_path = Rails.root.join('public', 'webpack_native', 'manifest.json')
+      JSON.parse(File.read(webpack_manifest_path))
+    rescue Errno::ENOENT
+      fail "The webpack manifest file does not exist. Hint: run webpack command."
+    end
+
     if Rails.env.production?
-      require_relative 'generators/webpack_native/templates/webpack_native_helper.rb'
-      Rails.configuration.x.webpack_native.webpack_manifest_file = WebpackNativeHelper.load_webpack_manifest
+      # require_relative 'generators/webpack_native/templates/webpack_native_helper.rb'
+      Rails.configuration.x.webpack_native.webpack_manifest_file = load_webpack_manifest
     end
   end
 
